1bffee593566f21d4cf838357f0f466d05e6d8ae
axiom-of-joy <xander@axiom-of-joy.com>
2019-06-29 23:55:59 +0000

    Altered generate.sh to make it more readable. Added input folder for user MIDI inputs.

--- generate.sh
+++ generate.sh
@@ -1,1 +1,6 @@
-python3 generate.py -s save/ecomp.sess -l 1000 -b 1 -g 0.7 -i dataset/midi/ecomp_piano/sun01.mid
+python3 generate.py \
+-s save/ecomp.sess \
+-l 1000 \
+-b 1 \
+-g 0.7 \
+-i dataset/midi/ecomp_piano/sun01.mid
