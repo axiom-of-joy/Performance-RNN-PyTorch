bb4b28c2e77d7572227bb5089fda15cf7f89f322
axiom-of-joy <xander@axiom-of-joy.com>
2019-06-30 03:55:47 +0000

    Further trained the LSTM for several hours. Does not produce good-sounding musical output.

--- generate.sh
+++ generate.sh
@@ -1,1 +1,5 @@
-python3 generate.py -s save/LSTM_model.sess -l 1000 -c '1,0,1,0,1,1,0,1,0,1,0,1;3'
+python3 generate.py \
+-s save/LSTM_model.sess \
+-l 1500 \
+-g 0.8 \
+#-c '1,0,1,0,1,1,0,1,0,1,0,1;3' \
--- train_lstm.sh
+++ train_lstm.sh
@@ -2,4 +2,4 @@ python3 train.py \
 -s save/LSTM_model.sess \
 -d dataset/processed/ecomp_piano \
 -i 10 \
-
+-L
