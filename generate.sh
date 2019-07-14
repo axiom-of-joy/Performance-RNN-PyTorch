04b379667315a22091aac79fecc83cc858e9669f
axiom-of-joy <xander@axiom-of-joy.com>
2019-07-14 04:02:36 +0000

    Mostly added documentation to quantize.py. Minor changes to generate.py, example.ipynb, and generate.sh. Still need to re-test quantize.py to ensure that changes did not break anything.

--- generate.sh
+++ generate.sh
@@ -3,5 +3,5 @@ python3 generate.py \
 -l 1000 \
 -b 1 \
 -g 0.7 \
--q performance_rnn_pretrained_stats.yaml \
+-q stats/performance_rnn_pretrained_stats.yaml \
 -i input/trimmed_sun01.mid
--- quantize.py
+++ quantize.py
@@ -1,38 +1,72 @@
 '''
-This script quantizes the PerformanceRNN model defined in model.py. It
-requires that the forked Distiller repo at
-https://github.com/axiom-of-joy/distiller is cloned inside of the
+This script defines a Quantizer class that may be used to quantize
+instances of PerformanceRNN. It requires that the forked Distiller repo
+at https://github.com/axiom-of-joy/distiller is cloned inside of the
 amadeus project folder and installed in development mode (see README.md
 for installation and set-up details).
+
+This script borrows from the example at
+https://github.com/NervanaSystems/distiller/blob/master/examples/word_language_model/quantize_lstm.ipynb.
 '''
 
-import os, sys, optparse
-import config
-from model import PerformanceRNN
-import torch
-from torch import nn
-import distiller
-from distiller.modules.gru import convert_model_to_distiller_gru
-from tqdm import tqdm
-import numpy as np
-from sequence import EventSeq, Control, ControlSeq
-from data import Dataset
-from sequence import EventSeq
-import torch.functional as F
-from distiller.quantization import PostTrainLinearQuantizer, LinearQuantMode
+import os
+import sys
+import optparse
 from copy import deepcopy
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.functional as F
+import numpy as np
+import distiller
+from distiller.quantization import PostTrainLinearQuantizer, LinearQuantMode
+from distiller.modules.gru import convert_model_to_distiller_gru
 from distiller.data_loggers import QuantCalibrationStatsCollector
 from distiller.data_loggers import collector_context
+import config
+from model import PerformanceRNN
+from sequence import EventSeq, Control, ControlSeq
+from sequence import EventSeq
+from data import Dataset
 
 #-----------------------------------------------------------------------
 # Quantizer class.
 #-----------------------------------------------------------------------
 
 class Quantizer:
-    def __init__(self, model: 'PerformanceRNN'):
+    """
+    A class that quantizes instances of PerformanceRNN.
+
+    Attributes:
+        model (nn.Module): An instance of PerformanceRNN.
+        batch_size (int): Size of batches used to collect
+            pre-quantization statistics.
+        num_batches (int): Number of batches used to collect
+            pre-quantization statistics.
+        use_transposition (bool): True if data should be transposed.
+        window_size (int): Length of window size.
+        teacher_forcing_ratio (float): The teacher forcing ratio.
+        control_ratio (float): The control ratio.
+    """
+
+    def __init__(self, model):
+        """
+        The constructor for the Quantizer class.
+
+        Args:
+            model (nn.Module): An instance of PerformanceRNN.
+
+        Returns:
+            None
+        """
+
+        # Convert the instances of nn.modules.GRU in the input model to
+        # instances of DistillerGRU.
         self.model = deepcopy(model)
         convert_model_to_distiller_gru(self.model)
-
+        
+        # Load attributes from config.py.
+        self.batch_size = config.collect_quant_stats['batch_size']
         self.num_batches = config.collect_quant_stats['num_batches']
         self.use_transposition = (
             config.collect_quant_stats['use_transposition'])
@@ -40,34 +74,36 @@ class Quantizer:
         self.teacher_forcing_ratio = (
             config.collect_quant_stats['teacher_forcing_ratio'])
         self.control_ratio = config.collect_quant_stats['control_ratio']
-        self.batch_size = config.collect_quant_stats['batch_size']
 
     def collect_stats(self, stats_file, batch_gen):
         """
-        Collects pre-quantization calibration statistics for the
-        self.model.
+        Collects pre-quantization calibration statistics for self.model.
 
-        Loads num_batches batches of data from batch_gen and collects
-        pre-quantization statistics for self.model. Writes the results
-        to stats_file.
+        Loads num_batches batches of data from batch_gen and monitors the
+        distribution of model weights during the forward pass of the data
+        through self.model. The resulting statistics are written to stats_file.
 
         Args:
             stats_file (str): Path to YAML file where statistics will be
                 written.
 
-            batch_gen (dataset.batches): Batch generator from data file.
+            batch_gen (dataset.batches): An instance of the batch generator
+                defined in data.py.
 
         Returns:
             None
         """
+
+        # Set up pre-quantization calibration statistics collector.
         distiller.utils.assign_layer_fq_names(self.model)
         collector = QuantCalibrationStatsCollector(self.model)
         device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-        with collector_context(collector) as collector:
-            for iteration, (events, controls) in enumerate(batch_gen):
-                print(iteration)
+        print("Collecting pre-quantization calibration statistics.")
 
+        with collector_context(collector) as collector:
+            for iteration, (events, controls) in tqdm(enumerate(batch_gen)):
+                # Break when desired number of batches are processed.
                 if iteration == self.num_batches:
                     break
 
@@ -95,11 +131,11 @@ class Quantizer:
                     teacher_forcing_ratio=self.teacher_forcing_ratio,
                     output_type='softmax')
 
-
                 assert outputs.shape[:2] == events.shape[:2]
 
-            # Save stats.
+            # Save statistics.
             collector.save(stats_file)
+
 
     def quantize(self, stats_file):
         """
@@ -111,11 +147,14 @@ class Quantizer:
                 pre-quantization statistics.
 
         Returns:
-            quantizer(PostTrainLinearQuantizer): Quantizer object with
+            quantizer (PostTrainLinearQuantizer): Quantizer object with
                 quantized model stored at quantizer.model.
         """
 
-        # The following overrides the default quantization routine.
+        # The following override ensures that element-wise addition and
+        # multiplication operations inside of instances of DistillerGRU are not
+        # quantized but rather are carried out in half-precision. This has a
+        # significant impact on the performance of the quantized model.
         overrides_yaml = """
         .*eltwise.*:
             fp16: true
@@ -128,10 +167,10 @@ class Quantizer:
             model_activation_stats=stats_file,
             overrides=overrides,
             mode=LinearQuantMode.ASYMMETRIC_SIGNED,
-            per_channel_wts=True
+            per_channel_wts=True,
+            clip_acts='AVG'
         )
-
         quantizer.prepare_model()
-        quantizer.model.eval()  # Disables dropout.
+        quantizer.model.eval()
         return quantizer
 
